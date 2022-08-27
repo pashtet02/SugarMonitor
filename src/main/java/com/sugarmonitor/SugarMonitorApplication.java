a0f3f089393c4f8022e06534d38cc7a19b1c0097
pashtetgusia <chodak2014ua@gmail.com>
2022-08-27 23:32:29 +0300

    just trying to deploy on heroku

---
+++ src/main/java/com/sugarmonitor/SugarMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.sugarmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SugarMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sugarmonitor/SugarMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sugarmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugarMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
