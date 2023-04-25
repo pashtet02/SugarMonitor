2241d53860b823bfc900a76b47c456793d95670f
pashtetgusia <chodak2014ua@gmail.com>
2023-04-25 22:50:26 +0300

    decided to parce JSON as String mannyally

--- src/main/java/com/sugarmonitor/controller/UploadController.java
+++ src/main/java/com/sugarmonitor/controller/UploadController.java
@@ -1,5 +1,8 @@
 package com.sugarmonitor.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sugarmonitor.model.Entry;
 import com.sugarmonitor.repos.EntryRepository;
 import java.util.*;
@@ -14,7 +17,14 @@ public class UploadController {
   private final EntryRepository entryRepository;
 
   @PostMapping("/upload")
-  public @ResponseBody ResponseEntity<Void> uploadDataFromPhone(@RequestBody List<Entry> entries) {
+  public @ResponseBody ResponseEntity<Void> uploadDataFromPhone(@RequestBody String requestJSON) {
+    Collection<Entry> entries = null;
+    try {
+      entries =
+          new ObjectMapper().readValue(requestJSON, new TypeReference<Collection<Entry>>() {});
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
     System.out.println("\n\n\n\nENTRY list:::" + entries);
     entries.forEach(System.out::println);
     List<Entry> savedEntries = entryRepository.saveAll(entries);
