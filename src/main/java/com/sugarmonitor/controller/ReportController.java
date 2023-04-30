a6e5723dab1ba0db8f6e8a47db79f7bd7bb2be0d
pashtetgusia <chodak2014ua@gmail.com>
2023-04-30 22:38:08 +0300

    TEMP commit to not to lose changes

--- src/main/java/com/sugarmonitor/controller/ReportController.java
+++ src/main/java/com/sugarmonitor/controller/ReportController.java
@@ -4,6 +4,11 @@ import com.sugarmonitor.model.Entry;
 import com.sugarmonitor.model.Profile;
 import com.sugarmonitor.service.GraphService;
 import com.sugarmonitor.service.ProfileService;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 import java.util.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -28,7 +33,7 @@ public class ReportController {
   }
 
   @GetMapping("/daytoday")
-  public String mainReportMenu(
+  public String generateDayToDayReports(
       @RequestParam(name = "generateFor", required = false, defaultValue = "") String generateFor,
       @RequestParam(name = "fromDate", required = false)
           @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
@@ -90,6 +95,94 @@ public class ReportController {
     model.addAttribute("profile", activeProfile);
     model.addAttribute("map", map);
     return "report";
+  }
+
+  @GetMapping("/weektoweek")
+  public String generateWeekToWeekReports(
+      @RequestParam(name = "generateFor", required = false, defaultValue = "") String generateFor,
+      @RequestParam(name = "fromDateWeek", required = false)
+          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+          Date fromDateWeek,
+      @RequestParam(name = "toDateWeek", required = false)
+          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+          Date toDateWeek,
+      Model model) {
+
+    Calendar fromCal = Calendar.getInstance();
+    fromCal.set(Calendar.HOUR_OF_DAY, 0);
+    fromCal.set(Calendar.MINUTE, 0);
+    fromCal.set(Calendar.SECOND, 0);
+
+    Calendar toCal = Calendar.getInstance();
+    toCal.set(Calendar.HOUR_OF_DAY, 23);
+    toCal.set(Calendar.MINUTE, 59);
+    toCal.set(Calendar.SECOND, 59);
+
+    LocalDateTime monday = getStartOfPreviousMonday();
+
+    Date from;
+    Date to;
+    fromCal.add(Calendar.DATE, -21);
+    from = fromCal.getTime();
+    to = toCal.getTime();
+
+    List<Entry> data = graphService.findByDateBetween(from.getTime(), to.getTime());
+    Map<String, List<Map<String, Double>>> map =
+        groupEntriesByWeeks(data, profileService.getProfile());
+
+    Profile activeProfile = profileService.getProfile();
+
+    model.addAttribute("lowSugarLine", profileService.getLowerBoundLimit());
+    model.addAttribute("highSugarLine", profileService.getHighBoundLimit());
+    model.addAttribute("yAxisGraphMinLimit", profileService.getYAxisGraphMinLimit());
+    model.addAttribute("yAxisGraphMaxLimit", profileService.getYAxisGraphMaxLimit());
+    model.addAttribute("yAxisGraphStep", profileService.getYAxisGraphStep());
+    model.addAttribute("profile", activeProfile);
+    model.addAttribute("weekMap", map);
+
+    return "report";
+  }
+
+  // Helper method to get the start of the previous Monday
+  private static LocalDateTime getStartOfPreviousMonday() {
+    return LocalDateTime.now()
+        .with(TemporalAdjusters.previous(DayOfWeek.MONDAY))
+        .withHour(0)
+        .withMinute(0)
+        .withSecond(0);
+  }
+
+  public Map<String, List<Map<String, Double>>> groupEntriesByWeeks(
+      List<Entry> entries, Profile activeProfile) {
+    Map<String, List<Map<String, Double>>> result = new LinkedHashMap<>();
+    for (Entry entry : entries) {
+      String day = entry.getDateString().replace("T", " ").split(" ")[0];
+
+      LocalDate entryWrittenDate = LocalDate.parse(day);
+      LocalDate mondayOfWeek =
+          entryWrittenDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+      LocalDate sundayOfWeek =
+          entryWrittenDate.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
+      String week =
+          mondayOfWeek.format(DateTimeFormatter.ofPattern("yy-MM-dd"))
+              + ":"
+              + sundayOfWeek.format(DateTimeFormatter.ofPattern("yy-MM-dd"));
+      result
+          .computeIfAbsent(
+              week,
+              k -> {
+                LinkedList<Map<String, Double>> emptyList = new LinkedList<>();
+                for (int i = 0; i < 7; i++) {
+                  emptyList.add(new LinkedHashMap<>());
+                }
+                return emptyList;
+              })
+          .get(entryWrittenDate.getDayOfWeek().getValue() - 1)
+          .put(
+              graphService.convertEntryDateIntoStringOnGraph(entry),
+              entry.getSgv(activeProfile.getUnits()));
+    }
+    return result;
   }
 
   public Map<String, Map<String, Double>> groupEntriesByDay(
